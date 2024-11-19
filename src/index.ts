411fd0058162c0e0c1024f37bdff6d01a4e73536
Aryan Mehta <aryanmehta2016@gmail.com>
2024-11-19 08:48:14 +0000

    fixign port issue

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import app from "./app";
 
 dotenv.config();
 
+console.log(process.env.SERVER_PORT);
+
 app.listen(process.env.SERVER_PORT || 8080, () => {
   console.log("SERVER STARTED ON PORT: ", process.env.SERVER_PORT);
   return "Hello";
